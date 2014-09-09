bd8ccfb498e2172cc7dde370d545fc73b10fc6e5
Akash Waran <akash.waran@gmail.com>
2014-09-09 18:29:24 -0400

    Added java docs and organized imports of internal folder

--- app/src/main/java/com/example/android/notepad/internal/ICryptUtils.java
+++ app/src/main/java/com/example/android/notepad/internal/ICryptUtils.java
@@ -7,8 +7,75 @@ import java.io.IOException;
  */
 public interface ICryptUtils {
 
+    /**
+     *
+     * This method is used to encrypt a notepad file, it automatically hashes the
+     * result and stores it along with the file for integrity check.
+     * Cipher used - AES block cipher (namely CBC)
+     * Hashing function - SHA-1 Hash with 128 bits key
+     *
+     * @param iv    - Initialization vector required for cbc block cipher
+     * @param key   - Key used to encrypt string
+     * @param data  - String to be encrypted
+     * @param salt  - salt used for hashing result requried for integrity check
+     *
+     * @return  Encrypted string
+     *          Returns null on failure
+     */
     public String stringEncrypt(byte[] iv, byte[] key, String data, String salt);
+
+    /**
+     *
+     * This method is used to decrypt a notepad file, it first verifies the hash
+     * for integrity and if the hash matches proceeds to decrypt the string.
+     * Cipher used - AES block cipher (namely CBC)
+     * Hashing function - SHA-1 Hash with 128 bits key
+     *
+     * @param iv    - Initialization vector required for cbc block cipher
+     * @param key   - Key used to decrypt string
+     * @param data  - String to be decrypted
+     * @param salt  - salt used for hashing result requried for integrity check
+     *
+     * @return  Decrypted string
+     *          Returns null on failure
+     */
     public String stringDecrypt(byte[] iv, byte[] key, String data, String salt);
+
+    /**
+     *
+     * This method is used to encrypt a notepad file given path, it first verifies
+     * for integrity and if the hash matches proceeds to encrypt the file.
+     * Cipher used - AES block cipher (namely CBC)
+     * Hashing function - SHA-1 Hash with 128 bits key
+     *
+     * @param iv    - Initialization vector required for cbc block cipher
+     * @param key   - Key used to decrypt string
+     * @param path  - Path of file to be encrypted
+     * @param salt  - salt used for hashing result requried for integrity check
+     *
+     * @return  Encrypted string
+     *          Returns null on failure
+     *
+     * @throws IOException
+     */
     public String fileEncrypt(byte[] iv, byte[] key, String path, String salt) throws IOException;
+
+    /**
+     *
+     * This method is used to decrypt a notepad file given path, it first verifies
+     * for integrity and if the hash matches proceeds to decrypt the file.
+     * Cipher used - AES block cipher (namely CBC)
+     * Hashing function - SHA-1 Hash with 128 bits key
+     *
+     * @param iv    - Initialization vector required for cbc block cipher
+     * @param key   - Key used to decrypt string
+     * @param path  - Path of file to be decrypted
+     * @param salt  - salt used for hashing result requried for integrity check
+     *
+     * @return  Decrypted string
+     *          Returns null on failure
+     *
+     * @throws IOException
+     */
     public String fileDecrypt(byte[] iv, byte[] key, String path, String salt) throws IOException;
 }
--- app/src/main/java/com/example/android/notepad/internal/IUtilities.java
+++ app/src/main/java/com/example/android/notepad/internal/IUtilities.java
@@ -4,6 +4,32 @@ package com.example.android.notepad.internal;
  * Created by Akash on 9/9/2014.
  */
 public interface IUtilities {
+
+    /**
+     *
+     * This method can be used to generate a random byte array.
+     * This is useful for generating salt or initializing vectors.
+     *
+     * Note : This method makes use of secure random
+     *
+     * @param size - The number of bytes of data you want to randomly generate
+     *
+     * @return Random byte array
+     */
     public byte[] generateRandom(int size);
+
+    /**
+     *
+     * This method is used to generate the key used for encryption.
+     * Due to the nature of key we need this to be truely random and it is
+     * thus generated using a seed provided by the user.
+     * This is done to ensure randomness by using a turely random seed.
+     *
+     * Note : This function is used where the randomness of the result is of utmost importance
+     *
+     * @param seed - Random seed which we use to generate a random key.
+     *
+     * @return Random byte array
+     */
     public byte[] generateRawKey(byte[] seed);
 }
